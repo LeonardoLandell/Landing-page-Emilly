b09b4c32c123f63b309f894aeadc4bd647600c09
LeonardoLandell <leonardolandell2000@gmail.com>
2026-04-17 17:23:21 -0300

    Referencias

--- app/components/Services.jsx
+++ app/components/Services.jsx
@@ -4,43 +4,33 @@ export default function Services() {
       <h2>Serviços</h2>
       <div className="cards">
         <div className="card">
-          <img src="/images/icone-flor.png" alt="Massagem Relaxante" />
           <h3>Massagem Relaxante</h3>
           <p>
-            Movimentos suaves e deslizantes para relaxanmento muscular geral.
-            <br></br>
-            reduzindo estresse e ansiedade.
+            Movimentos suaves e deslizantes para relaxamento muscular geral.
+            <br />
+            Reduzindo estresse e ansiedade.
           </p>
         </div>
         <div className="card">
-          <img src="/images/icone-flor.png" alt="Massagem Qualificada" />
           <h3>Massagem Modeladora/Linfática</h3>
           <p>
-            Massagem de movimentos leves ou firmes repetetitivos que reduzem a
-            retenção de liquido,<br></br> inchaço e modelam o corpo.
+            Massagem de movimentos leves ou firmes repetitivos que reduzem a
+            retenção de líquido,
+            <br /> inchaço e modelam o corpo.
           </p>
         </div>
         <div className="card">
-          <img
-            src="/images/icone-flor.png"
-            alt="Massagem em cadeira especial e ágil"
-          />
           <h3>Massagem Rápida</h3>
           <p>
-            Terapia revigorante mais ágil, focada en costas, pescoço e ombros.
-            <br></br>
+            Terapia revigorante mais ágil, focada em costas, pescoço e ombros.
+            <br />
             Promovendo alívio imediato.
           </p>
         </div>
         <div className="card">
-          <img
-            src="/images/icone-flor.png"
-            alt="Massagem com técnica japnesa"
-          />
           <h3>Shiatsu</h3>
           <p>
-            {" "}
-            Técnica japonesa que utiliza pressão com os dedos e palmas <br></br>
+            Técnica japonesa que utiliza pressão com os dedos e palmas <br />
             em meridianos de energia para liberar tensões e dores físicas.
           </p>
         </div>
--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -7,9 +7,7 @@ export default function Footer() {
     <footer className="footer">
       <div className="footer-content">
         {/* Logo */}
-        <div className="footer-logo">
-          <img src="/images/logo.png" alt="Logo" />
-        </div>
+        <div className="footer-logo"></div>
 
         {/* Links com ícones */}
         <div className="footer-links">
